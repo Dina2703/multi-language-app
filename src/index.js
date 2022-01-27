b8b9e63c75578334c40e4c55d385b109e73b64fd
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-27 12:05:30 -0500

    i18next-http-backend utility and setup

--- src/index.js
+++ src/index.js
@@ -3,45 +3,36 @@ import ReactDOM from "react-dom";
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
 import "./index.css";
 // import App from "./App";
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(LanguageDetector)
+  .use(HttpApi)
   .init({
     // the translations
     // (tip move them in a JSON file and import them,
     // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-    resources: {
-      en: {
-        translation: {
-          "Welcome to React": "Welcome to React",
-        },
-      },
-      ru: {
-        translation: {
-          "Welcome to React": "Добро пожаловать в React",
-        },
-      },
-      kz: {
-        translation: {
-          "Welcome to React": "Қош келдіңіз React-қа",
-        },
-      },
-    },
 
     fallbackLng: "en",
     detection: {
-      order: ["htmlTag", "cookie", "localStorage", "path", "subdomain"],
+      order: ["cookie", "htmlTag", "localStorage", "path", "subdomain"],
       caches: ["cookie"],
+    },
+    backend: {
+      loadPath: "/assets/locales/{{lng}}/translation.json",
+    },
+    react: {
+      useSuspense: false,
     },
   });
 
 function App() {
   const { t } = useTranslation();
 
-  return <h2>{t("Welcome to React")}</h2>;
+  return <h2>{t("welcome_to_react")}</h2>;
 }
 
 // append app to dom
