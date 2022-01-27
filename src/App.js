04b8e5cf517a93a09a54c34ce0e4ee51a3fb2af7
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-27 15:47:24 -0500

    added bootstrap and some context to App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useTranslation } from "react-i18next";
 function App() {
+  const { t } = useTranslation();
+  const releaseDate = new Date("2022-01-27");
+  const timeDifference = new Date() - releaseDate;
+  const number_of_days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="d-flex flex-column align-items-start">
+        <h1 className="font-weight-normal my-3"> {t("welcome_message")}</h1>
+      </div>
+      <p>{t("days_since_release", { number_of_days })}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
+import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
-import "./index.css";
-// import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+import App from "./App";
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(LanguageDetector)
   .use(HttpApi)
   .init({
+    supportedLngs: ["en", "ru", "kz"],
     // the translations
     // (tip move them in a JSON file and import them,
     // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
@@ -28,12 +31,6 @@ i18n
       useSuspense: false,
     },
   });
-
-function App() {
-  const { t } = useTranslation();
-
-  return <h2>{t("welcome_to_react")}</h2>;
-}
 
 // append app to dom
 ReactDOM.render(<App />, document.getElementById("root"));
