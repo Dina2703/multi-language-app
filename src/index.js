a496276819f7215256d1e8ec2efd065e271bb121
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-27 11:18:57 -0500

    i18next-browser-languagedetector utility and setup

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import "./index.css";
 // import App from "./App";
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
   .init({
     // the translations
     // (tip move them in a JSON file and import them,
@@ -28,8 +30,12 @@ i18n
         },
       },
     },
-    lng: document.querySelector("html").lang, // if you're using a language detector, do not define the lng option
+
     fallbackLng: "en",
+    detection: {
+      order: ["htmlTag", "cookie", "localStorage", "path", "subdomain"],
+      caches: ["cookie"],
+    },
   });
 
 function App() {
